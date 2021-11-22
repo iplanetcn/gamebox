c38a8756d32906e08687fdeb51223df427f7849c
john <iplanetcn@gmail.com>
2021-11-22 23:49:18 +0800

    update project structure

---
+++ googleads/src/main/java/cherry/gamebox/googleads/MainActivity.kt
@@ -0,0 +1,11 @@
+package cherry.gamebox.googleads
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
