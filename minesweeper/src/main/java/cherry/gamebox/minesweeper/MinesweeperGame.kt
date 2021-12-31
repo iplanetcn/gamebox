1c9e932f55485a44730d27cde85b96fc64879937
John <iplanetcn@gmail.com>
2021-12-31 09:06:34 +0800

    add MinesweeperGame

---
+++ minesweeper/src/main/java/cherry/gamebox/minesweeper/MinesweeperGame.kt
@@ -0,0 +1,20 @@
+package cherry.gamebox.minesweeper
+
+import com.badlogic.gdx.Game
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.scenes.scene2d.Stage
+
+/**
+ * Solitaire
+ *
+ * @author john
+ * @since 2021-12-09
+ */
+class MinesweeperGame : Game() {
+    lateinit var batch: SpriteBatch
+    lateinit var stage: Stage
+
+    override fun create() {
+
+    }
+}
