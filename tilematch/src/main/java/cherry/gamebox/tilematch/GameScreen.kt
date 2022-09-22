a48ea19ee5c4006741c2ca34b6f247a7caa7e7b9
John <iplanetcn@gmail.com>
2022-09-22 09:46:38 +0800

    update tilematch

--- tilematch/src/main/java/cherry/gamebox/tilematch/GameScreen.kt
+++ tilematch/src/main/java/cherry/gamebox/tilematch/GameScreen.kt
@@ -1,12 +1,17 @@
 package cherry.gamebox.tilematch
 
-import cherry.gamebox.core.GameLogger
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.ScreenAdapter
-import com.badlogic.gdx.graphics.GL30
+import com.badlogic.gdx.math.Interpolation
+import com.badlogic.gdx.scenes.scene2d.Group
 import com.badlogic.gdx.scenes.scene2d.InputEvent
-import com.badlogic.gdx.scenes.scene2d.InputListener
+import com.badlogic.gdx.scenes.scene2d.Stage
+import com.badlogic.gdx.scenes.scene2d.actions.Actions
 import com.badlogic.gdx.scenes.scene2d.ui.Image
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
+import com.badlogic.gdx.utils.Align
+import com.badlogic.gdx.utils.ScreenUtils
+import com.badlogic.gdx.utils.viewport.FitViewport
 
 /**
  * GameScreen
@@ -15,45 +20,71 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image
  * @since 2022-09-19
  */
 class GameScreen(private val game: TileMatchGame) : ScreenAdapter() {
-    private var block: Image
+    private var stage: Stage = Stage(FitViewport(SCREEN_WIDTH, SCREEN_HEIGHT))
+    private var heap: MutableList<Image> = mutableListOf()
+    private var stack: MutableList<Image> = mutableListOf()
+    private val group = Group()
 
     init {
-        val first = Assets.atlasRegionCakeList.first()
-        block = Image(first)
-        block.setSize(100f, 100f)
-        block.setPosition(SCREEN_WIDTH / 2 - block.width / 2, SCREEN_HEIGHT / 2 - block.height / 2)
-        game.stage.addActor(block)
-        Gdx.input.inputProcessor = game.stage
-        game.stage.addListener(object : InputListener() {
-            override fun touchDown(
-                event: InputEvent?,
-                x: Float,
-                y: Float,
-                pointer: Int,
-                button: Int
-            ): Boolean {
-                GameLogger.log("touchDown: $x, $y; pointer: $pointer; button: $button" )
-                return true
-            }
-        })
+        Gdx.input.inputProcessor = stage
+        spawnActor()
+        stage.addActor(group)
     }
 
     override fun render(delta: Float) {
         // clear
-        Gdx.gl.glClearColor(0.294f, 0.294f, 0.294f, 1f)
-        Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT)
-
+        ScreenUtils.clear(0.294f, 0.294f, 0.294f, 1f)
         game.camera.update()
-
         // draw
         game.batch.begin()
         game.batch.end()
-        game.stage.act(delta)
-        game.stage.draw()
+        stage.act(delta)
+        stage.draw()
 
     }
 
     override fun resize(width: Int, height: Int) {
-        game.stage.viewport.update(width, height, true)
+        stage.viewport.update(width, height, true)
+    }
+
+    private fun spawnActor() {
+        Assets.atlasRegionFruitList.forEach { fruitTexture ->
+            val img = Image(fruitTexture)
+            img.setBounds(0f, 0f, 40f, 40f)
+            img.setPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, Align.center)
+            img.setOrigin(img.width / 2, img.height / 2)
+            heap.add(img)
+        }
+
+        var i = 0
+        var j = 0
+        heap.forEach { image ->
+            if (i == 8) {
+                i = 0
+                j++
+            }
+            image.setPosition(i * 60f, j * 60f)
+            image.addListener(object : ClickListener() {
+                override fun clicked(event: InputEvent?, x: Float, y: Float) {
+                    val actor = event?.listenerActor
+                    actor?.addAction(
+                        Actions.sequence(
+                            Actions.parallel(
+                                Actions.rotateTo(720f, 1f, Interpolation.linear),
+                                Actions.moveTo(group.x + stack.size * 60f, 100 - group.y, 0.5f)
+                            ),
+//                            Actions.fadeOut(0.3f),
+//                            Actions.removeActor()
+                        )
+                    )
+                    heap.remove(actor)
+                    stack.add(actor as Image)
+                }
+            })
+            group.addActor(image)
+            i++
+        }
+
+        group.setPosition(SCREEN_WIDTH / 2 - 240, SCREEN_HEIGHT - 480)
     }
 }
--- tilematch/src/main/java/cherry/gamebox/tilematch/TileMatchGame.kt
+++ tilematch/src/main/java/cherry/gamebox/tilematch/TileMatchGame.kt
@@ -2,11 +2,9 @@ package cherry.gamebox.tilematch
 
 import cherry.gamebox.core.GameLogger
 import com.badlogic.gdx.Game
-import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.scenes.scene2d.Stage
-import com.badlogic.gdx.utils.viewport.StretchViewport
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 
 /**
  * TileMatchGame
@@ -14,26 +12,24 @@ import com.badlogic.gdx.utils.viewport.StretchViewport
  * @author john
  * @since 2022-09-19
  */
-val SCREEN_WIDTH = Gdx.graphics.width.toFloat()
-val SCREEN_HEIGHT = Gdx.graphics.height.toFloat()
+const val SCREEN_WIDTH = 480f
+const val SCREEN_HEIGHT = 720f
 
 class TileMatchGame : Game() {
     lateinit var batch: SpriteBatch
-    lateinit var stage: Stage
     lateinit var camera: OrthographicCamera
+    lateinit var shapeRenderer: ShapeRenderer
     var paused: Boolean = false
 
     override fun create() {
         GameLogger.log("create()")
         // 将LibGDX日志级别设定为DEBUG
         GameLogger.setLogDebug()
-        stage = Stage(
-            StretchViewport(SCREEN_WIDTH, SCREEN_HEIGHT)
-        )
         camera = OrthographicCamera(SCREEN_WIDTH, SCREEN_HEIGHT)
         camera.position.set(SCREEN_WIDTH / 2f, SCREEN_HEIGHT / 2f, 0f)
         Assets.load()
         batch = SpriteBatch()
+        shapeRenderer = ShapeRenderer()
         setScreen(GameScreen(this))
         // 启动时默认激活游戏
         paused = false
@@ -54,8 +50,8 @@ class TileMatchGame : Game() {
     override fun dispose() {
         super.dispose()
         GameLogger.log("dispose()")
-        stage.dispose()
         batch.dispose()
+        shapeRenderer.dispose()
         Assets.dispose()
     }
 }
